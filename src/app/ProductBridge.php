a186c21086e7b571288835e087980d3e0eba4155
Eduardo Mejia <edumejia30@gmail.com>
2018-01-18 18:13:49 -0400

    Correcciones parciales

--- src/app/ProductBridge.php
+++ src/app/ProductBridge.php
@@ -3,12 +3,23 @@
 namespace Solunes\Business\App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 
 class ProductBridge extends Model {
 	
 	protected $table = 'product_bridges';
 	public $timestamps = true;
 
+    use Sluggable, SluggableScopeHelpers;
+    public function sluggable(){
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+	
 	/* Creating rules */
 	public static $rules_create = array(
 		'name'=>'required',
@@ -25,9 +36,21 @@ class ProductBridge extends Model {
 		'city_id'=>'required',
 		'address'=>'required',
 	);
-    
+
     public function city() {
         return $this->belongsTo('Solunes\Business\App\City');
+    }
+
+    public function getRealPriceAttribute() {
+        $price = $this->price;
+        if($offer = $this->product_offer){
+            if($offer->type=='discount_percentage'){
+                $price = $price - ($price * $offer->value / 100);
+            } else if($offer->type=='discount_value'){
+                $price = $price - $offer->value;
+            }
+        }
+        return $price;
     }
 
 }
